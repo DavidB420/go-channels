ce644815df10e7af774ed51abc2c92a53ff5a871
David Badiei <DavidB420@users.noreply.github.com>
2023-08-08 11:52:05 -0400

    Added buffered channels, for loop, and close channel

---
+++ buffered-channel/buffered-channel.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+        "fmt"
+)
+
+func main(){
+
+        fmt.Println("Buffered Go channel Example")
+
+        //Make a buffered channel
+        bufChannel := make (chan int, 2)
+
+        //Send two consecutive values to the buffered channel
+		bufChannel <- 65535
+		bufChannel <- 12345
+
+        //Output the received messages
+        fmt.Println(<-bufChannel)
+		fmt.Println(<-bufChannel)
+
+}
---
+++ forloop-channel/forloop-channel.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	fmt.Println("For Loop Channel Example")
+
+	//Create a buffered channel with a capacity of 3
+	bufChannel := make(chan int, 3)
+
+	//Send three values to the channel
+	bufChannel <- 65535
+	bufChannel <- 12345
+	bufChannel <- 67890
+
+	//Close the channel
+	close(bufChannel)
+	
+	//Iterate through all the items in the channel
+	for item := range bufChannel {
+		fmt.Println(item)
+	}
+
+}
