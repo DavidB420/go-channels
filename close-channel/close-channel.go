bc73361d6877324b9b4c139a23c9e923c0dd91ab
David Badiei <DavidB420@users.noreply.github.com>
2023-08-08 11:51:22 -0400

    Added buffered channels, for loop, and close channel

---
+++ close-channel/close-channel.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	fmt.Println("Channel close Example")
+
+	//Created a buffered channel with a capacity of 2
+	bufChannel := make(chan int, 2)
+
+	//Send two values to the buffered channel
+	bufChannel <- 65535
+	bufChannel <- 12345
+
+	//Close the channel
+	close(bufChannel)
+
+	//Receive the two values after closing the channel
+	fmt.Println(<-bufChannel)
+	fmt.Println(<-bufChannel)
+
+	//Indicate that the channel is now closed
+	fmt.Println("Channel is now closed.")
+
+}
